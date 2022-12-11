d6145708134896504bc17aeaf9993d9dc67555b2
Rishab Jain <rishab1065@gmail.com>
2022-12-11 12:20:11 +0530

    updated 2021 inputs

--- 2021/Day14/part1.js
+++ 2021/Day14/part1.js
@@ -1,46 +1,46 @@
 const fs = require('fs');
 const input = fs
-	.readFileSync('./input.txt', { encoding: 'utf8', flag: 'r' })
-	.trim()
-	.split('\n');
+  .readFileSync('./input.txt', { encoding: 'utf8', flag: 'r' })
+  .trim()
+  .split('\n');
 
 const map = {};
 
 input.forEach((line) => {
-	const part1 = line.split('->')[0].trim();
-	map[part1] = line.split('->')[1].trim();
+  const part1 = line.split('->')[0].trim();
+  map[part1] = line.split('->')[1].trim();
 });
 
 // console.log(map, 'input');
 let steps = 10;
-let line = 'OFSVVSFOCBNONHKFHNPK';
+let line = 'VOKKVSKKPSBVOOKVCFOV';
 
 while (steps--) {
-	for (let i = 0; i < line.length; ) {
-		if (i === line.length - 1) {
-			break;
-		}
-		if (map[`${line[i]}${line[i + 1]}`]) {
-			// console.log(map[`${line[i]}${line[i + 1]}`]);
+  for (let i = 0; i < line.length; ) {
+    if (i === line.length - 1) {
+      break;
+    }
+    if (map[`${line[i]}${line[i + 1]}`]) {
+      // console.log(map[`${line[i]}${line[i + 1]}`]);
 
-			line =
-				line.substring(0, i + 1) +
-				map[`${line[i]}${line[i + 1]}`] +
-				line.substring(i + 1);
+      line =
+        line.substring(0, i + 1) +
+        map[`${line[i]}${line[i + 1]}`] +
+        line.substring(i + 1);
 
-			i = i + 2;
-		} else {
-			i++;
-		}
-	}
-	console.log(steps);
+      i = i + 2;
+    } else {
+      i++;
+    }
+  }
+  console.log(steps);
 }
 console.log(line.length);
 let count = {};
 line.split('').forEach((ele) => {
-	if (!count[ele]) {
-		count[ele] = 0;
-	}
-	count[ele]++;
+  if (!count[ele]) {
+    count[ele] = 0;
+  }
+  count[ele]++;
 });
 console.log(count);
