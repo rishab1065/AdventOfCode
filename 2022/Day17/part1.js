3dd14baace23cd72e36709e206a1a3709a308507
Rishab Jain <rishab1065@gmail.com>
2022-12-18 19:58:32 +0530

    day 17 partial

--- 2022/Day17/part1.js
+++ 2022/Day17/part1.js
@@ -6,6 +6,8 @@ let input = fs
   .trim()
   .split('');
 
+const MAX_FLOOR = 10000;
+
 const movement = {
   '<': { dx: 0, dy: -1 },
   '>': { dx: 0, dy: 1 },
@@ -26,27 +28,27 @@ const rocks = [
     height: 3,
     width: 3,
     pos: [
+      [0, 1],
+      [1, 0],
+      [1, 1],
+      [1, 2],
+      [2, 1],
+    ],
+  },
+  {
+    height: 2,
+    width: 2,
+    pos: [
+      [0, 0],
+      [0, 1],
       [0, 2],
-      [-1, 1],
-      [-1, 2],
-      [-1, 3],
-      [0, 2],
+      [1, 2],
+      [2, 2],
     ],
   },
   {
     height: 3,
     width: 3,
-    pos: [
-      [0, 2],
-      [1, 2],
-      [2, 0],
-      [2, 1],
-      [2, 2],
-    ],
-  },
-  {
-    height: 4,
-    width: 1,
     pos: [
       [0, 0],
       [1, 0],
@@ -55,8 +57,8 @@ const rocks = [
     ],
   },
   {
-    height: 2,
-    width: 2,
+    height: 4,
+    width: 1,
     pos: [
       [0, 0],
       [0, 1],
@@ -66,7 +68,7 @@ const rocks = [
   },
 ];
 
-console.log(input, 'input');
+// console.log(input, 'input');
 
 let currentAirPressureIndex = 0;
 
@@ -74,20 +76,17 @@ let rocksStopped = 0;
 
 let rocksPosMap = {};
 
-let rockMap = new Array(5).fill().map(() => new Array(7).fill('.'));
+let rockMap = new Array(MAX_FLOOR).fill().map(() => new Array(7).fill('.'));
 
-let rockDepth = 0;
+let floor = 0;
+let maxHeight = 0;
+let currentRockPos = updateRockPos(rocks[0].pos, floor + 3, 2);
 
-let currentRockPos = updateRockPos(rocks[0].pos, 0, 2);
-
-console.log(currentRockPos, 'currentRockPos');
-while (rocksStopped < 2) {
+// console.log(currentRockPos, 'currentRockPos');
+while (rocksStopped < 2022) {
   const currentAirPressure = input[currentAirPressureIndex % input.length];
+  let isRockStoppedV = false;
 
-  let isRockStoppedH = false,
-    isRockStoppedV = false;
-
-  console.log(currentAirPressure, rockDepth, 'currentAirPressure');
   if (
     canRockMove(
       currentRockPos,
@@ -102,70 +101,73 @@ while (rocksStopped < 2) {
       movement[currentAirPressure].dx,
       movement[currentAirPressure].dy
     );
-
-    if (movement[currentAirPressure].dy === 1) {
-      console.log(currentRockPos, 'currentRockPos right');
-    } else {
-      console.log(currentRockPos, 'currentRockPos left');
-    }
-
-    isRockStoppedH = false;
+    // if (movement[currentAirPressure].dy === 1) {
+    //   console.log(currentRockPos, 'currentRockPos right');
+    // } else {
+    //   console.log(currentRockPos, 'currentRockPos left');
+    // }
   } else {
-    console.log('not moving ');
-    isRockStoppedH = true;
+    // console.log('not moving left or right');
   }
 
-  if (canRockMove(currentRockPos, rocksPosMap, 1, 0)) {
+  if (canRockMove(currentRockPos, rocksPosMap, -1, 0)) {
     // down
-    currentRockPos = updateRockPos(currentRockPos, 1, 0);
-
-    console.log(currentRockPos, 'currentRockPos down');
-    rockDepth++;
+    currentRockPos = updateRockPos(currentRockPos, -1, 0);
+    // console.log(currentRockPos, 'currentRockPos down');
+    //   //   rockDepth++;
     isRockStoppedV = false;
   } else {
-    console.log('not moving down');
+    // console.log('not moving down');
     isRockStoppedV = true;
   }
 
   // rocksStopped++;
-
-  console.log(isRockStoppedV, isRockStoppedH, 'isRockStopped');
-
+  // console.log(isRockStoppedV, 'isRockStopped');
   currentAirPressureIndex++;
-  if (rockDepth > 3 || (isRockStoppedV && isRockStoppedH)) {
+  if (isRockStoppedV) {
+    // console.log(currentRockPos, 'currentRockPos');
     for (let [x, y] of currentRockPos) {
       rockMap[x][y] = '#';
       rocksPosMap[`${x},${y}`] = true;
     }
     rocksStopped++;
-    rockDepth = 0;
-    console.log(currentRockPos, 'currentRockPos');
-    // nextRockPos
-    currentRockPos = updateRockPos(rocks[rocksStopped % 5].pos, 0, 2);
+    floor = currentRockPos.reduce((maxX, [x]) => Math.max(maxX, x), floor) + 1;
 
-    console.log(currentRockPos, rocksStopped, rockDepth, rocksPosMap);
+    isRockStoppedV = false;
+    // nextRockPos
+    currentRockPos = updateRockPos(rocks[rocksStopped % 5].pos, floor + 3, 2);
+    console.log({ floor });
   }
 }
 
-console.log('object');
+// console.log('object');
 
 function canRockMove(rockPos, rocksPosMap, dx, dy) {
   const newPos = updateRockPos(rockPos, dx, dy);
-  const [minY, maxY] = newPos.reduce(
-    ([minY, maxY], pos) => {
-      return [Math.min(minY, pos[1]), Math.max(maxY, pos[1])];
+  const [minX, minY, maxY] = newPos.reduce(
+    ([minX, minY, maxY], pos) => {
+      return [
+        Math.min(minX, pos[0]),
+        Math.min(minY, pos[1]),
+        Math.max(maxY, pos[1]),
+      ];
     },
-    [9, -1, -1]
+    [9, 9, -1]
   );
 
+  // console.log(floor, minX, minY, maxY, 'minX');
+  if (minX < 0) {
+    return false;
+  }
+
   if (minY < 0 || maxY > 6) {
-    console.log('helloo 1');
+    // console.log('helloo 1');
     return false;
   }
 
   for (let [x, y] of newPos) {
     if (rocksPosMap[`${x},${y}`]) {
-      console.log(x, y, 'helloo 2');
+      // console.log(x, y, 'helloo 2');
       return false;
     }
   }
@@ -177,6 +179,10 @@ function updateRockPos(rockPos, dx, dy) {
   return rockPos.map(([x, y]) => [x + dx, y + dy]);
 }
 
+rockMap = rockMap.reverse();
 for (let x = 0; x < rockMap.length; x++) {
-  console.log(rockMap[x].join(''));
+  console.log(rockMap[x].join('') + ' ' + (MAX_FLOOR - x - 1));
 }
+
+// console.log({ rocksPosMap });
+// console.log({ floor });
